777a223da6a3cbac41760323901f76013dd0f8c2
Jason Yates <yatesj9@gmail.com>
2024-11-18 13:42:56 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Time Away
+
+React, TypeScript, Vite and Tailwind frontend for [timeaway_api](https://github.com/yatesj9/timeaway_api)
+
+### Dev Server
+```
+npm run dev
+```
+
---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import "./App.css";
+
+export const url = {
+  api: "http://localhost:8085/api/",
+};
+
+// export const url = {
+//   api: "http://SERVERNAME:8085/api/",
+// };
+
+
+function App() {
+  return (
+    <>
+      <h1>Time Away</h1>
+    </>
+  );
+}
+
+export default App;
